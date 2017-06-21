2b8fa6ebafc79086a4ba397cecaa2a1c70740570
0xFireball <0xFireball@outlook.com>
2017-06-21 16:03:46 -0700

    tilemap api usage

--- Sources/PlayState.hx
+++ Sources/PlayState.hx
@@ -6,6 +6,7 @@ import n4.NState;
 import n4.group.NTypedGroup;
 import n4.NCamera;
 import n4.display.NBackdrop;
+import n4.tile.*;
 
 import sprites.*;
 
@@ -14,12 +15,31 @@ class PlayState extends NState {
 	private var player:Player;
 	private var boxes:NTypedGroup<Box>;
 
+	private var tilesetImage:NGraphic;
+
 	override public function create() {
 
 		bgColor = Color.fromFloats(0.95, 0.95, 0.95);
 
 		// var backdrop = new NBackdrop("backdrop.png");
 		// add(backdrop);
+
+		// load map
+		var gameTileset = new NTileset("simple_tset.png", 32, 32);
+		var map = NTilemapLoader.loadTilemapFromArray([
+			0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+			0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+			0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+			0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+			0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+			0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+			0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+			0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+			0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+			0,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,
+			0,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0
+		], gameTileset);
+		add(map);
 
 		player = new Player(240, 240);
 		add(player);
